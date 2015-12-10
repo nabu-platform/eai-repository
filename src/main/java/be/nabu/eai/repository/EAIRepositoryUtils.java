79db8941b3303e4f1e73ae340885c6053cfb19c2
alex <alex@nabu.be>
2015-12-10 13:07:16 +0100

    Added

--- src/main/java/be/nabu/eai/repository/EAIRepositoryUtils.java
+++ src/main/java/be/nabu/eai/repository/EAIRepositoryUtils.java
@@ -1,9 +1,13 @@
 package be.nabu.eai.repository;
 
 import java.io.IOException;
+import java.io.OutputStream;
+import java.nio.file.attribute.FileTime;
 import java.text.ParseException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -12,9 +16,18 @@ import be.nabu.eai.repository.api.Entry;
 import be.nabu.eai.repository.api.ExtensibleEntry;
 import be.nabu.eai.repository.api.Node;
 import be.nabu.eai.repository.api.Repository;
+import be.nabu.eai.repository.api.ResourceEntry;
 import be.nabu.eai.repository.api.ResourceRepository;
 import be.nabu.libs.artifacts.api.Artifact;
+import be.nabu.libs.resources.api.FiniteResource;
+import be.nabu.libs.resources.api.ReadableResource;
+import be.nabu.libs.resources.api.Resource;
+import be.nabu.libs.resources.api.ResourceContainer;
+import be.nabu.libs.resources.api.TimestampedResource;
 import be.nabu.libs.types.ParsedPath;
+import be.nabu.utils.io.IOUtils;
+import be.nabu.utils.io.api.ByteBuffer;
+import be.nabu.utils.io.api.ReadableContainer;
 
 public class EAIRepositoryUtils {
 
@@ -76,5 +89,59 @@ public class EAIRepositoryUtils {
 			path = path.getChildPath();
 		}
 		return entry;
+	}
+	
+	public static void zip(OutputStream output, Entry entry, EntryFilter acceptor) throws IOException {
+		ZipOutputStream zip = new ZipOutputStream(output);
+		try {
+			zipEntry(zip, entry, acceptor);
+		}
+		finally {
+			zip.finish();
+		}
+	}
+	
+	private static void zipEntry(ZipOutputStream output, Entry entry, EntryFilter acceptor) throws IOException {
+		if (entry instanceof ResourceEntry && entry.isNode() && (acceptor == null || acceptor.accept((ResourceEntry) entry))) {
+			zipNode(output, entry.getId().replace(".", "/"), ((ResourceEntry) entry).getContainer(), ((ResourceEntry) entry).getRepository());
+		}
+		if (!entry.isLeaf() && (acceptor == null || acceptor.recurse((ResourceEntry) entry))) {
+			for (Entry child : entry) {
+				if (child instanceof ResourceEntry) {
+					zipEntry(output, (ResourceEntry) child, acceptor);
+				}
+			}
+		}
+	}
+	
+	private static void zipNode(ZipOutputStream output, String path, ResourceContainer<?> container, ResourceRepository repository) throws IOException {
+		for (Resource resource : container) {
+			String childPath = path + "/" + resource.getName();
+			if (resource instanceof ReadableResource) {
+				ZipEntry entry = new ZipEntry(childPath);
+				if (resource instanceof FiniteResource) {
+					entry.setSize(((FiniteResource) resource).getSize());
+				}
+				if (resource instanceof TimestampedResource) {
+					entry.setLastModifiedTime(FileTime.fromMillis(((TimestampedResource) resource).getLastModified().getTime()));
+				}
+				output.putNextEntry(entry);
+				ReadableContainer<ByteBuffer> readable = ((ReadableResource) resource).getReadable();
+				try {
+					IOUtils.copyBytes(readable, IOUtils.wrap(output));
+				}
+				finally {
+					readable.close();
+				}
+			}
+			else if (resource instanceof ResourceContainer && repository.isInternal((ResourceContainer<?>) resource)) {
+				zipNode(output, childPath, (ResourceContainer<?>) resource, repository);
+			}
+		}
+	}
+	
+	public static interface EntryFilter {
+		public boolean accept(ResourceEntry entry);
+		public boolean recurse(ResourceEntry entry);
 	}
 }
