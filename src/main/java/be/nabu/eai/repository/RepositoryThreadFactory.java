6857bf1ec67219900259fd75471b958c2468950b
Alexander Verbruggen <alex@nabu.be>
2021-12-04 15:20:04 +0100

    added support for fork join thread factories

--- src/main/java/be/nabu/eai/repository/RepositoryThreadFactory.java
+++ src/main/java/be/nabu/eai/repository/RepositoryThreadFactory.java
@@ -1,11 +1,14 @@
 package be.nabu.eai.repository;
 
 import java.util.concurrent.Executors;
+import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.ForkJoinPool.ForkJoinWorkerThreadFactory;
+import java.util.concurrent.ForkJoinWorkerThread;
 import java.util.concurrent.ThreadFactory;
 
 import be.nabu.eai.repository.api.Repository;
 
-public class RepositoryThreadFactory implements ThreadFactory {
+public class RepositoryThreadFactory implements ThreadFactory, ForkJoinWorkerThreadFactory {
 	
 	private Repository repository;
 	private ThreadFactory parent;
@@ -38,5 +41,16 @@ public class RepositoryThreadFactory implements ThreadFactory {
 	}
 	public void setName(String name) {
 		this.name = name;
+	}
+
+	@Override
+	public ForkJoinWorkerThread newThread(ForkJoinPool pool) {
+		ForkJoinWorkerThread thread = ForkJoinPool.defaultForkJoinWorkerThreadFactory.newThread(pool);
+		thread.setDaemon(daemonize);
+		thread.setContextClassLoader(repository.getClassLoader());
+		if (name != null) {
+			thread.setName(name);
+		}
+		return thread;
 	}
 }
